1d68cbeb30a3cba0766754850443728365985f08
MOHAMMED NIHAL N <69244384+mohdnihaln@users.noreply.github.com>
2024-11-27 21:26:34 +0530

    Add files via upload

---
+++ 1.dart
@@ -0,0 +1,21 @@
+void main(){
+
+String Name = "Mohammed Nihal N"; //character
+
+int age = 22; //number without dots
+
+var degreeScore = 7.45; //float number , var complie time assigner
+
+final isAStudent; //runtime assigner
+
+isAStudent = true; //re-assigning
+
+const studying = "at Techmindz"; //constant value which cannot be changed
+
+dynamic number = 6282914567; // dynamic is also a runtime assigner
+
+num contact = 8547454783;
+
+print("MR.$Name is a $age old person. Who is a Diploma graduated with the score of $degreeScore and he is a Student at $studying. His phone number is $number. His alternative number is $contact ");
+
+}
