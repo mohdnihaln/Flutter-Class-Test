a51425d1c1023dc7990eca836857e8cf2ec127a9
MOHAMMED NIHAL N <69244384+mohdnihaln@users.noreply.github.com>
2024-11-29 14:53:10 +0530

    Add files via upload

---
+++ 3_Equality&Relational_Operators.dart
@@ -0,0 +1,53 @@
+void main(){
+
+var a = 20;
+
+var b = 10;
+
+var c = 10;
+
+var d = 30;
+
+if(b==c){ //equal to
+  print("b is equal to c");
+}
+else{
+  print("b is not equal to c");
+}
+
+if(a!=b){ //not equal to
+  print("a is not equal to b");
+}
+else{
+  print("a is equal to b");
+}
+
+if(a>b){ //greater than
+  print("a is greater than b");
+}
+else{
+  print("a is not greater than b");
+}
+
+if(a<b){ //smaller than
+  print("a is smaller than b");
+}
+else{
+  print("a is not smaller than b");
+}
+
+if(a<=b){ //smaller or eqal to
+  print("a is smaller or equal to b");
+}
+else{
+  print("a is not smaller or equal to b");
+}
+
+if(c>=b){ //greater or equal to
+  print("c is greater or equal to b");
+}
+else{
+  print("c is not greater or equal to b");
+}
+
+}
