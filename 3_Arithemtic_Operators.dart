0430e48ef6bc26416c12c67ff17a3cbc9ff3ea04
MOHAMMED NIHAL N <69244384+mohdnihaln@users.noreply.github.com>
2024-11-29 14:39:45 +0530

    Add files via upload

---
+++ 3_Arithemtic_Operators.dart
@@ -0,0 +1,41 @@
+void main(){
+
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var add = a + b ; //addition
+
+print(add);
+
+var sub = a - c ; //substraction
+
+print(sub);
+
+var div = a / b ; //division
+
+print(div);
+
+var division = a ~/ c ; //division witout float value
+
+print(division);
+
+var mul = a * b ; //multiplication
+
+print(mul);
+
+var modulus = a % b ; //modulus
+
+print(modulus);
+
+var increment = ++a; //increment
+
+print(increment);
+
+var decrement = --b; //decrement
+
+print(decrement);
+
+}
